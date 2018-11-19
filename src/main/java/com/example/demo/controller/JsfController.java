fa1719e5f161189bf0955d7b58a4835ac7daba52
Gabriel <contato.gabrielgarcia@gmail.com>
2018-11-19 16:11:04 -0200

    First Commit

---
+++ src/main/java/com/example/demo/controller/JsfController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Scope(value = "session")
+@Component(value = "jsfController")
+public class JsfController {
+
+    @Autowired
+    public String loadTodoPage() {
+        checkPermission();
+        return "/todo.xhtml";
+    }
+
+    private void checkPermission() {
+    }
+}
